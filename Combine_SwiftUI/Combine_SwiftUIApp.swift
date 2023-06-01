c58b7b0232276c6ed1195a8d10914a7e7fd8b587
Sunil Kumar <sunil.sm@1mg.com>
2023-06-01 16:41:00 +0530

    Initial Commit

---
+++ Combine_SwiftUI/Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_SwiftUIApp.swift
+//  Combine_SwiftUI
+//
+//  Created by Sunil Kumar on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
